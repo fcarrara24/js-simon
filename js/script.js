09779be0d120ee91dce50e3f8a59e92a662953a6
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-20 16:47:32 +0200

    creating a utility function to work with all clasasname

--- js/script.js
+++ js/script.js
@@ -10,9 +10,59 @@ const arraySequenze = document.getElementsByClassName('sequencePrinted');
 btn.addEventListener('click', game());
 
 function game() {
+    //getting the output array to print results in
+    const outputArray = document.getElementsByClassName('sequencePrinted');
+    console.log('fsdf ' + outputArray.length)
+    let randomArray = generatenumbers(outputArray.length);
+
+    //printing values
+    assigningPrintValues();
+
+    //timer to hide values
+    setTimeout(() => {
+        console.log('stop')
+        classListManager('\'sequencePrinted\'', 'd-none', 'add');
+    }, 3000); // to change, now 3s
 
 
 
+    //              //
+    //  functions   //
+    //              //
+
+    /**
+     * giving or removing specific classes for all items
+     * @param {} className 
+     * @param {*} propriety 
+     * @param {*} action 
+     * @returns 
+     */
+    function classListManager(className, propriety, action) {
+        const classItems = document.getElementsByClassName(className);
+        console.log(classItems)
+        console.log(classItems.length)
+        for (let i = 0; i < classItems.length; i++) {
+
+            switch (action) {
+                case 'add':
+                    classItems[i].add(`'${propriety}'`)
+                    break;
+                case 'remove':
+                    classItems[i].remove(`'${propriety}'`)
+                    break;
+                case 'toggle':
+                    classItems[i].toggle(`'${propriety}'`)
+                    break;
+            }
+        }
+        return;
+
+    }
+    function assigningPrintValues() {
+        for (let i = 0; i < outputArray.length; i++) {
+            outputArray[i].innerHTML = randomArray[i];
+        }
+    }
     /**
      * return an array containig 5 rnd generated numbers
      * @param {*} totalnumbers 
