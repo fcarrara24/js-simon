ea61e1975690255e8057d550e229ace3759f465c
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-20 17:57:51 +0200

    adjusted classlistmanager and added a check for values to two array to coincide

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
-const btn = document.querySelector('#generateBtn');
-const inputEl = document.getElementById('data');
-const resultEl = document.querySelector('.alert');
+const btnSend = document.getElementById('send');
+const btn = document.getElementById('generateBtn');
 
-const arraySequenze = document.getElementsByClassName('sequencePrinted');
+let outputArray = document.getElementsByClassName('sequencePrinted');
 
 
 
 
 btn.addEventListener('click', game());
+btnSend.addEventListener('click', () => {
+    let Inputuser = (classListManager('data', '', 'value'));
+    console.log(Inputuser)
+    let sequence = classListManager('sequencePrinted', '', 'inner')
+
+    console.log(sequence)
+    let output = classListManager('alert', '', 'get')[0];
+    console.log('dsafasd')
+    if (checking()) {
+        output.innerHTML = 'grande, hai una memoria di ferro'
+    } else {
+        output.innerHTML = 'che peccato, il fosforo ti aiutera a ricordare'
+    }
+    //remove d-none
+    output.classList.remove('d-none');
+
+    function checking() {
+        console.log('a')
+        console.log(sequence.length)
+        if (Inputuser.length !== sequence.length) {
+            return false
+        }
+        for (let i = 0; i < Inputuser.length; i++) {
+            if (!(sequence.includes(Inputuser[i]))) {
+                return false;
+            }
+        }
+        return true;
+    }
+});
 
 function game() {
+
     //getting the output array to print results in
-    const outputArray = document.getElementsByClassName('sequencePrinted');
-    console.log('fsdf ' + outputArray.length)
+    outputArray = document.getElementsByClassName('sequencePrinted');
     let randomArray = generatenumbers(outputArray.length);
 
     //printing values
@@ -21,8 +50,10 @@ function game() {
     //timer to hide values
     setTimeout(() => {
         console.log('stop')
-        classListManager('\'sequencePrinted\'', 'd-none', 'add');
-    }, 3000); // to change, now 3s
+        classListManager('sequencePrinted', 'd-none', 'add');
+        //alòlows user read
+
+    }, 30000); // to change, now 3s
 
 
 
@@ -30,38 +61,12 @@ function game() {
     //  functions   //
     //              //
 
-    /**
-     * giving or removing specific classes for all items
-     * @param {} className 
-     * @param {*} propriety 
-     * @param {*} action 
-     * @returns 
-     */
-    function classListManager(className, propriety, action) {
-        const classItems = document.getElementsByClassName(className);
-        console.log(classItems)
-        console.log(classItems.length)
-        for (let i = 0; i < classItems.length; i++) {
 
-            switch (action) {
-                case 'add':
-                    classItems[i].add(`'${propriety}'`)
-                    break;
-                case 'remove':
-                    classItems[i].remove(`'${propriety}'`)
-                    break;
-                case 'toggle':
-                    classItems[i].toggle(`'${propriety}'`)
-                    break;
-            }
-        }
-        return;
-
-    }
     function assigningPrintValues() {
         for (let i = 0; i < outputArray.length; i++) {
             outputArray[i].innerHTML = randomArray[i];
         }
+        console.log(outputArray[0].innerHTML)
     }
     /**
      * return an array containig 5 rnd generated numbers
@@ -84,9 +89,62 @@ function game() {
     * @returns {Integer}
     */
     function rndInt(min, max) {
+
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
 
+
+//utility function
+/**
+     * giving or removing specific classes for all items
+     * @param {} className 
+     * @param {*} propriety 
+     * @param {*} action 
+     * @returns 
+     */
+function classListManager(className, propriety, action) {
+    const classItems = document.getElementsByClassName(className);
+    const getReturn = [];
+    for (let i = 0; i < classItems.length; i++) {
+        switch (action) {
+            case 'add':
+                classItems[i].classList.add(propriety);
+                break;
+            case 'remove':
+                classItems[i].classList.remove(propriety);
+                break;
+            case 'toggle':
+                classItems[i].classList.toggle(propriety);
+                break;
+            case 'get':
+                getReturn.push(classItems[i]);
+                break
+            case 'value':
+                getReturn.push(classItems[i].value);
+                break
+            case 'inner':
+                console.log('inner ' + classItems[i].innerHTML)
+                getReturn.push(classItems[i].innerHTML);
+
+                break
+            default:
+                console.error('Azione non valida:', action);
+        }
+    }
+    return getReturn;
+}
